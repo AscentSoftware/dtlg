25b1ff1b3ceeb3db530812e93478102332043fbe
Ramiro Magno <ramiro.morgado@ascent.io>
2025-07-30 01:37:01 +0100

    Add `is_string()` and `and()`
    `and()` is a variadic version of `&`

--- R/predicates.R
+++ R/predicates.R
@@ -5,3 +5,12 @@ is_bool <- function(x) {
 is_scalar_number <- function(x) {
   is.numeric(x) && identical(length(x), 1L) && !is.na(x)
 }
+
+is_string <- function(x) {
+  is.character(x) && identical(length(x), 1L) && !is.na(x)
+}
+
+and <- function(...) {
+  dots <- list(...)
+  Reduce(`&`, dots, init = TRUE)
+}
